1e7178a0708c9a4804de126ece0eb7c3fd4dcbd7
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 23:59:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,11 @@ def my_select(collection)
   filtered_collection = []
   i = 0
   while i < collection.length
+<<<<<<< HEAD
     filtered_collection << collection[i] if yield(collection[i])
+=======
+    filtered_collection << yield(collection[i])
+>>>>>>> 1b7d6ec357d4266a88428365632e0378ad405c47
     i = i+1
   end
   filtered_collection
@@ -15,5 +19,9 @@ arr = [1,2,3,4,5]
 my_select(arr) do |num|
   if num.even?
     num
+<<<<<<< HEAD
+=======
+    binding.pry
+>>>>>>> 1b7d6ec357d4266a88428365632e0378ad405c47
   end
 end
